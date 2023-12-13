3d062962a917c889864581cb7da155d611532f3f
Mateusz Juszczak <mateusz.juszczak@protonmail.com>
2023-12-13 22:52:55 +0100

    Implemented python minizinc driver and corrected minizinc problem description.

--- MinizincDriver.py
+++ MinizincDriver.py
@@ -1,26 +1,27 @@
 import minizinc
 import json
 import sys
+import numpy as np
 
-def process_json_data(json_data):
-    model = minizinc.Model("knapsack.mzn")
-    data = minizinc.Instance(json_data)
+def runMinizinc(json_data):   
+    model = minizinc.Model("TwoConstraintKnapsack.mzn")
+    #ddata = minizinc.Instance(json_data)
     solver = minizinc.Solver.lookup("gecode")
-    instance = minizinc.Instance(solver, model, data=data)
+    instance = minizinc.Instance(solver, model)
+    instance["maxsize"] = int(json_data["maxsize"])
+    instance["maxweight"] = int(json_data["maxweight"])
+    instance["n"] = int(json_data["n"])
+    instance["sizes"] = np.array(json_data["sizes"])
+    instance["weights"] = np.array(json_data["weights"])
+    instance["values"] = np.array(json_data["values"])
     result = instance.solve()
-
-    print(result)
+    print(result.objective)
 
 if __name__ == "__main__":
     try:
-        # Read JSON data from stdin
         stdin_data = sys.stdin.read()
-
-        # Load JSON data
         json_data = json.loads(stdin_data)
-
-        # Process JSON data
-        process_json_data(json_data)
+        runMinizinc(json_data)
 
     except json.JSONDecodeError as e:
         print("Error decoding JSON:", e)

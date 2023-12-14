23a8cefb2bcca796572ec9e99a10ebd3a01ccdba
Mateusz Juszczak <mateusz.juszczak@protonmail.com>
2023-12-14 16:57:03 +0100

    Moved MinizincDriver and MiniZinc model to build.

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ import subprocess
 import matplotlib.pyplot as plt
 import json
 
+
+
 iter_range = 100
 num_items = 20
+
+def generateTest()
 
 for iter in range(0, iter_range):
   test_case = subprocess.run(f"./testgen --numitems = {num_items} --seedrand={iter}", capture_output=True, text=True)
